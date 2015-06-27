e7c03b6f189190879fb48559c3ea84dc27d607c1
arthur29 <arthur29@homtail.com.br>
2015-06-27 13:41:35 -0300

    Models feitos e banco de dados alterado com ultimas correções

---
+++ Model/Receita.class.php
@@ -0,0 +1,47 @@
+<?php
+class Receita{
+	private $idReceita = 0;
+	private $nomeReceita = "";
+	private $descricaoReceita = "";
+	private $dificuldadeReceita = "";
+	private $publicaReceita = false;
+	
+	public function __construct(){
+		$this->idReceita = 0;
+		$this->nomeReceita = "";
+		$this->descricaoReceita = "";
+		$this->dificuldadeReceita = "";
+		$this->publicaReceita = false;
+	}
+	
+	public function getIdReceita (){
+		return $this->idReceita;
+	}
+	public function getNomeReceita (){
+		return $this->nomeReceita;
+	}
+	public function getDescricaoReceita (){
+		return $this->idReceita;
+	}
+	public function getDificuldadeReceita (){
+		return $this->dificuldadeReceita;
+	}
+	public function getPublicaReceita (){
+		return $this->publicaReceita;
+	}
+	public function setIdReceita ($int){
+		$this->idReceita = $int;
+	}
+	public function setNomeReceita ($string){
+		$this->nomeReceita = $string;
+	}
+	public function setDescricaoReceita ($string){
+		$this->descricaoReceita = $string;
+	}
+	public function setDificuldadeReceita ($string){
+		$this->dificuldadeReceita = $string;
+	}
+	public function setPublicaReceita ($bool){
+		$this->publicaReceita = $bool;
+	}
+}
---
+++ Model/Usuario.class.php
@@ -0,0 +1,55 @@
+<?php
+class Usuario {
+	private $idUsuario = 0;
+	private $nomeUsuario = "";
+	private $sobreNomeUsuario = "";
+	private $loginUsuario = "";
+	private $senhaUsuario = "";
+	private $emailUsuario = "";
+	
+	public function __construct (){
+		$this->idUsuario = 0;
+		$this->nomeUsuario = "";
+		$this->sobreNomeUsuario = "";
+		$this->loginUsuario = "";
+		$this->senhaUsuario = "";
+		$this->emailUsuario = "";
+	}
+	
+	public function getIdUsuario (){
+		return $this->idUsuario;
+	}
+	public function getNomeUsuario (){
+		return $this->nomeUsuario;
+	}
+	public function getSobreNomeUsuario (){
+		return $this->sobreNomeUsuario;
+	}
+	public function getLoginUsuario (){
+		return $this->loginUsuario;
+	}
+	public function getSenhaUsuario (){
+		return $this->senhaUsuario;
+	}
+	public function getEmailUsuario (){
+		return $this->emailUsuario;
+	}
+	public function setIdUsuario ($int){
+		$this->idUsuario = $int;
+	}
+	public function setNome ($string){
+		$this->nomeUsuario = $string;
+	}
+	public function setSobreNomeUsuario ($string){
+		$this->sobreNomeUsuario = $string;
+	}
+	public function setLoginUsuario ($string){
+		$this->loginUsuario = $string;
+	}
+	public function setSenhaUsuario ($string){
+		$this->senhaUsuario = $string;
+	}
+	public function setEmailUsuario ($string){
+		$this->emailUsuario = $string;
+	}
+}
